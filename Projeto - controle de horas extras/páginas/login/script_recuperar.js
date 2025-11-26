6f3bbfc6b6efe69cae402ceb5b9b17f5d842eda3
Thales Chequer <thales.chequer10@gmail.com>
2025-11-26 12:13:17 -0300

    Delete Projeto - controle de horas extras/páginas directory

--- Projeto - controle de horas extras/páginas/login/script_recuperar.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('formRecuperar').addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    const email = document.getElementById('email').value;
-
-    alert('Um código de verificação foi enviado para: ' + email);
-});
